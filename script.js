997ee1ee77e8987cd4813e71ff4db63fcbefc2f1
Tjebbe <tjebbemarchand@icloud.com>
2020-01-22 21:33:26 +0100

    Added initial javascript code

---
+++ script.js
@@ -0,0 +1,9 @@
+const choices = document.querySelectorAll(".choice");
+const score = document.querySelector("#score");
+const result = document.querySelector("#result");
+const restart = document.querySelector("#restart");
+const modal = document.querySelector(".modal");
+const scoreBoard = {
+    player: 0,
+    computer: 0
+};
