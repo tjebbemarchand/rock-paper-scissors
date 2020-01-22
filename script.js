31c70334a5d6ad3e851161e748c4917dd0cbd791
Tjebbe <tjebbemarchand@icloud.com>
2020-01-22 21:40:44 +0100

    Get user and computer choice

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ const scoreBoard = {
     player: 0,
     computer: 0
 };
+
+// Play game.
+function playGame(e) {
+    restart.style.display = "inline-block";
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+}
+
+// Computer choice.
+function getComputerChoice() {
+    const rand = Math.floor(Math.random() * choices.length + 1);
+
+    if (rand === 1) {
+        return "rock";
+    } else if (rand === 2) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
+
+choices.forEach(choice => choice.addEventListener("click", playGame));
